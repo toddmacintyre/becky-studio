faf1499deec1910d57b423e593b7f7386352e42d
Todd MacIntyre <toddcmacintyre@gmail.com>
2018-02-11 22:26:06 -0800

    add css transitions for react components

--- app/components/App.js
+++ app/components/App.js
@@ -6,16 +6,18 @@ import Home from './Home';
 import About from './About';
 import Classes from './Classes';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Wave />
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/classes" component={Classes} />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Wave />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/classes" component={Classes} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h2 id="heading">Home</h2>
-    </div>
-  );
-};
+// const Home = () => {
+//   return (
+//     <div>
+//       <h2 id="heading">Home</h2>
+//     </div>
+//   );
+// };
+
+class Home extends React.Component {
+  componentDidMount() {
+    const el = ReactDOM.findDOMNode(this);
+    setTimeout(() => {
+      el.className = 'loaded';
+    }, 0);
+  }
+
+  render() {
+    return (
+      <div className="loading" >
+        <h2 id="heading">Home</h2>
+      </div>
+    );
+  }
+}
 
 export default Home;
